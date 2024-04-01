f2cd5c19741f1586b4f019708f13054c9bfa24f2
Konstantin Krivilev <inferno750@mail.ru>
2024-04-01 20:12:35 +0300

    Navbar widget, ui component Link

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -2,13 +2,13 @@ type Mods = Record<string, boolean | string>;
 
 export function classNames(
   cls: string,
-  mod: Mods,
-  additional: string[]
+  mods: Mods = {},
+  additional: string[] = []
 ): string {
   return [
     cls,
-    ...additional,
-    ...Object.entries(mod)
+    ...additional.filter(Boolean),
+    ...Object.entries(mods)
       .filter(([className, value]) => Boolean(value))
       .map(([className]) => className),
   ].join(" ");
