11b16985f363d0fe3dc49359d53dfe2f043855e0
Konstantin Krivilev <inferno750@mail.ru>
2024-04-04 22:12:00 +0300

    fix bug i18n Ally ext., hmr plugin

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 const MainPage = () => {
   const { t } = useTranslation("main");
 
-  return <div>{t("Главная страница")}</div>;
+  return <div>{t("mainPage")}</div>;
 };
 
 export default MainPage;
