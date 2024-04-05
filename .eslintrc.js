9f22a4a880295e74605a0eeed24028b589bf97de
Konstantin Krivilev <inferno750@mail.ru>
2024-04-05 20:45:37 +0300

    add stylelint, i18n plugin for eslint, fix all styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     extends: [
         'plugin:react/recommended',
         'airbnb',
+        'plugin:i18next/recommended',
     ],
     overrides: [
         {
@@ -28,6 +29,7 @@ module.exports = {
     plugins: [
         'react',
         '@typescript-eslint',
+        'i18next',
     ],
     rules: {
         'react/function-component-definition': 'off',
@@ -45,6 +47,7 @@ module.exports = {
         'no-unused-vars': 'warn',
         'no-shadow': 'off',
         'no-underscore-dangle': 'off',
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
     },
     globals: {
         __IS_DEV__: true,
